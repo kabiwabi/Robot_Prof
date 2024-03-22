4b93df7395ab45e85ad62bbbbe2dcd7e99cb719a
Jon Micah M- <99670836+MillerSty@users.noreply.github.com>
2024-03-22 14:35:33 -0400

    Fuseki Finish

--- src/main.py
+++ src/main.py
@@ -6,13 +6,13 @@ import LectureAndTopicsBuilder as LTB
 from queries import Query
 from LectureAndTopicsBuilder import EX, URIRef, DBPEDIA
 
-
 SPARQLSERVER = False
 if (SPARQLSERVER):
     from src.queries import Fuseki_Queries as FQ
 
 # Define the namespaces
 vivo = Namespace("http://vivoweb.org/ontology/core#")
+
 
 def main():
     if (SPARQLSERVER):
@@ -42,24 +42,32 @@ def main():
     Query.execute_query(g, 2, "programming")  # Query 2: Find courses covering a specific topic
     Query.execute_query(g, 3, "COMP-479", 1)  # Query 3: Topics covered in a specific lecture of a course
     Query.execute_query(g, 4, "ConcordiaUniversity", "COMP")
-    Query.execute_query(g, 5, DBPEDIA.Intelligent_system, "COMP","474")  # Query 5: Materials recommended for a topic in a course
+    Query.execute_query(g, 5, DBPEDIA.Intelligent_system, "COMP",
+                        "474")  # Query 5: Materials recommended for a topic in a course
     Query.execute_query(g, 6, "COMP", "479")  # Query 6: Credits for a specific course
     Query.execute_query(g, 7, "COMP", "474")  # Query 7: Additional resources for a specific course
     Query.execute_query(g, 8, "COMP", "474", 1)  # Query 8: Content for a specific lecture in a course
-    Query.execute_query(g, 9, DBPEDIA.Information_retrieval, "COMP","479")  # Query 9: Reading materials for a topic in a course
+    Query.execute_query(g, 9, DBPEDIA.Information_retrieval, "COMP",
+                        "479")  # Query 9: Reading materials for a topic in a course
     Query.execute_query(g, 10, "stat", "380")  # Query 10: Competencies gained from a course
-    Query.execute_query(g, 11, "Ilise", "506","coms")  # Query 11: Get grade of a student who completed a course
+    Query.execute_query(g, 11, "Ilise", "506", "coms")  # Query 11: Get grade of a student who completed a course
     Query.execute_query(g, 12, "506", "Coms")  # Query 12: Students who completed a specific course
     Query.execute_query(g, 13, "Braun")  # Query 13: Print transcript for a student
 
     if SPARQLSERVER:
-        FQ.FusekiQuery1(sparql)
-        FQ.FusekiQuery2(sparql, "artificial intelligence")
-        FQ.FusekiQuery4(sparql, "conc", "coen")
-        FQ.FusekiQuery10(sparql, "stat", "380")
-        FQ.FusekiQuery11(sparql, "Ilise", "506", "coms")
-        FQ.FusekiQuery12(sparql, "506", "Coms")
-        FQ.FusekiQuery13(sparql, "Braun")
+        FQ.execute_fuseki_query(sparql, 1)
+        FQ.execute_fuseki_query(sparql, 2, "programming")
+        FQ.execute_fuseki_query(sparql, 3, "COMP-479", 1)
+        FQ.execute_fuseki_query(sparql, 4, "ConcordiaUniversity", "comp")
+        FQ.execute_fuseki_query(sparql, 5, DBPEDIA.Intelligent_system, "COMP", "474")
+        FQ.execute_fuseki_query(sparql, 6, "COMP", "479")
+        FQ.execute_fuseki_query(sparql, 7, "COMP", "474")
+        FQ.execute_fuseki_query(sparql, 8, "COMP", "474", 1)
+        FQ.execute_fuseki_query(sparql, 9, DBPEDIA.Information_retrieval, "COMP", "479")
+        FQ.execute_fuseki_query(sparql, 10, "stat", "380")
+        FQ.execute_fuseki_query(sparql, 11, "Ilise", "506", "coms")
+        FQ.execute_fuseki_query(sparql, 12, "506", "Coms")
+        FQ.execute_fuseki_query(sparql, 13, "Braun")
 
 
 if __name__ == '__main__':
